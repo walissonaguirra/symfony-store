ca889057600b75dcb9f9a2cc959fab7dfa8769fe
Walisson Aguirra <walissonaguirra@proton.me>
2025-09-10 11:25:36 -0300

    feat: cria um template index.html.twig para o DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
     #[Route('/default', name: 'app_default')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DefaultController.php',
-        ]);
+        return $this->render('index.html.twig');
     }
 }
